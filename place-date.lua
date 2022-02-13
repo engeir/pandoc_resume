df5454d4ed664e9adb48e0a3aaaaff0af9609002
engeir <eirroleng@gmail.com>
2022-02-13 14:31:01 +0100

    Improve age calculation

--- place-date.lua
+++ place-date.lua
@@ -5,7 +5,10 @@ function Str (s)
         -- s.text = text.upper(s.text)
     end
     if s.text == "myage" then
-        age = tonumber(os.date("%Y")) - 1996
+        reference = os.time{day=23, year=1996, month=5}
+        daysfrom = os.difftime(os.time(), reference) / (24 * 60 * 60) -- seconds in a day
+        wholeyears = math.floor(daysfrom / 365)
+        age = tonumber(wholeyears)
         s.text = age
     end
     return s
